89fc8adee57da80f7cbdc7d6eb5cb24cfcc60e9d
BRUCEBANNER00 <105909158+BRUCEBANNER00@users.noreply.github.com>
2022-09-05 16:17:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345678))
+    API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     BOT_NAME = os.environ.get("BOT_NAME")
+    PICS = (environ.get('PICS', 'https://telegra.ph/file/ebe8da68b5d8bfb0f4548.jpg https://telegra.ph/file/a7fbbb10d2f79af0de2b5.jpg https://telegra.ph/file/59dd925676b800f087879.jpg')
