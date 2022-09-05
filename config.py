a1469ec98a57dcea0d4e1ddb62a6773dddf3bad0
BRUCEBANNER00 <105909158+BRUCEBANNER00@users.noreply.github.com>
2022-09-05 16:52:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     BOT_NAME = os.environ.get("BOT_NAME")
     PICS = (environ.get('PICS', 'https://telegra.ph/file/ebe8da68b5d8bfb0f4548.jpg https://telegra.ph/file/a7fbbb10d2f79af0de2b5.jpg https://telegra.ph/file/59dd925676b800f087879.jpg')
-    FORCE_CHANNEL = environ.get('FORCE_CHANNEL')
+    FORCE_CHANNEL = environ.get('FORCE_CHANNEL', '-1001696817546')
